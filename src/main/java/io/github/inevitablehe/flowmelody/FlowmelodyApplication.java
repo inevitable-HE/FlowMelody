ed11489c65a74b5dfa357c3bda434b67f2553570
kevin yuan <yzy61111hhhh@163.com>
2025-01-12 23:54:16 +0800

    Initial commit

---
+++ src/main/java/io/github/inevitablehe/flowmelody/FlowmelodyApplication.java
@@ -0,0 +1,13 @@
+package io.github.inevitablehe.flowmelody;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowmelodyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlowmelodyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/inevitablehe/flowmelody/FlowmelodyApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.inevitablehe.flowmelody;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowmelodyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
